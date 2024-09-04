e9ef427d61b5a35188f9eb2d60855701041c1fc3
oblivion4us <nft4oblivion@gmail.com>
2024-09-04 14:39:17 +0200

    add abstract class Hero and 2 classes of Hero

--- main.py
+++ main.py
@@ -23,3 +23,47 @@ class PhysicalWeapon(Weapon):
 class MagicalWeapon(Weapon):
     def attack_type(self):
         return "magical"
+
+# Классы героев
+class Hero(ABC):
+    def __init__(self, name, health, ability, weapon):
+        self.name = name
+        self.health = health
+        self.ability = ability
+        self.weapon = weapon
+
+    @abstractmethod
+    def set_ability(self):
+        pass
+
+    def attack(self, enemy):
+        if self.health <= 0 or enemy.health <= 0:
+            return
+
+        multiplier = 1.4 if self.ability.lower() == "magical" and self.weapon.attack_type() == "magical" else 1.5 if self.ability.lower() == "physical" and self.weapon.attack_type() == "physical" else 1
+        damage = random.randint(self.weapon.min_damage, self.weapon.max_damage) * multiplier
+
+        print(f"{self.name} атакует {enemy.name}, нанося {damage:.1f} {self.weapon.attack_type()} урона!")
+        time.sleep(1)  # Задержка перед следующим сообщением
+        enemy.health -= damage
+        if enemy.health > 0:
+            print(f"У {enemy.name} осталось {enemy.health:.1f} кол-ва здоровья")
+        else:
+            enemy.health = 0
+            print(f"{enemy.name} погиб!")
+
+    def check_health(self):
+        if self.health <= 0:
+            print(f"{self.name} погиб!")
+            return True
+        return False
+
+class Wizard(Hero):
+    def set_ability(self):
+        print(f"{self.name} имеет аспект {self.ability}. Его урон от магии увеличен на 40%!")
+        time.sleep(1)  # Задержка перед следующим сообщением
+
+class Warrior(Hero):
+    def set_ability(self):
+        print(f"{self.name} имеет аспект {self.ability}. Его физический урон увеличен на 50%!")
+        time.sleep(1)  # Задержка перед следующим сообщением
